92356f732a67c22891140501d6ab2c8ef6b7c21f
Fiona Ochs <fiona.oaks@gmail.com>
2020-01-27 16:50:40 -0800

    initial data

---
+++ api.js
@@ -0,0 +1,102 @@
+export const productsData = [
+    {
+        id: 'bag',
+        name: 'bag',
+        image: './assets/assets/bag.jpg'
+    },
+    {
+        id: 'banana',
+        name: 'banana',
+        image: './assets/assets/banana.jpg'
+    },
+    {
+        id: 'bathroom',
+        name: 'bathroom',
+        image: './assets/assets/bathroom.jpg'
+    },
+    {
+        id: 'boots',
+        name: 'boots',
+        image: './assets/assets/boots.jpg'
+    },
+    {
+        id: 'breakfast',
+        name: 'breakfast',
+        image: './assets/assets/breakfast.jpg'
+    },
+    {
+        id: 'bubblegum',
+        name: 'bubblegum',
+        image: './assets/assets/bubblegum.jpg'
+    },
+    {
+        id: 'chair',
+        name: 'chair',
+        image: './assets/assets/chair.jpg'
+    },
+    {
+        id: 'cthulhu',
+        name: 'cthulhu',
+        image: './assets/assets/cthulhu.jpg'
+    },
+    {
+        id: 'dog-duck',
+        name: 'dog-duck',
+        image: './assets/assets/dog-duck.jpg'
+    },
+    {
+        id: 'dragon',
+        name: 'dragon',
+        image: './assets/assets/dragon.jpg'
+    },
+    {
+        id: 'pen',
+        name: 'pen',
+        image: './assets/assets/pen.jpg'
+    },
+    {
+        id: 'pet-sweep',
+        name: 'pet-sweep',
+        image: './assets/assets/pet-sweep.jpg'
+    },
+    {
+        id: 'scissors',
+        name: 'scissors',
+        image: './assets/assets/scissors.jpg'
+    },
+    {
+        id: 'shark',
+        name: 'shark',
+        image: './assets/assets/shark.jpg'
+    },
+    {
+        id: 'sweep',
+        name: 'sweep',
+        image: './assets/assets/sweep.jpg'
+    },
+    {
+        id: 'tauntaun',
+        name: 'tauntaun',
+        image: './assets/assets/tauntaun'
+    },
+    {
+        id: 'unicorn',
+        name: 'unicorn',
+        image: './assets/assets/unicorn.jpg'
+    },
+    {
+        id: 'usb',
+        name: 'usb',
+        image: './assets/assets/usb'
+    },
+    {
+        id: 'water-can',
+        name: 'water-can',
+        image: './assets/assets/water-can'
+    },
+    {
+        id: 'wine-glass',
+        name: 'wine-glass',
+        image: './assets/assets/wine-glass.jpg'
+    },
+];
---
+++ app.js
@@ -0,0 +1,1 @@
+import { productsData } from './api.js';
